bbc05ada2f239417809ccd3735a5a9d37245fe78
Stefano Campanella <stefano.campanella@usi.ch>
2024-02-07 16:08:27 +0100

    refactoring

--- bodegha.py
+++ bodegha.py
@@ -1,21 +1,30 @@
 import subprocess
 
 def run_dockerfile(token, repo):
-    # Run the Docker container and capture the output
+    """
+    Run the Docker container and capture the output
+    @param token: the GitHub token
+    @param repo: the GitHub repository
+
+    @return: the output of the Docker container
+    """
     result = subprocess.run(["docker", "run", "--rm", "bodegha", repo, "--key", token, "--csv"], capture_output=True, text=True)
 
     output_lines = result.stdout.split('\n') 
     output_lines.pop(0) # headers are not needed
 
-    parsed_output = [tuple(line.split(',')) for line in output_lines if line]
-    parsed_output = [t for t in parsed_output if t[1] != 'Unknown']
-    parsed_output = [(t[0], False) if t[1] in ['Human'] else t for t in parsed_output]
-    parsed_output = [(t[0], True) if t[1] in ['Bot'] else t for t in parsed_output] 
+    parsed = []
+    for line in output_lines:
+        if line:
+            line = line.split(',')
+            line[1] = line[1].strip()
+            if line[1] == 'Unknown':
+                continue
+            if line[1] == 'Human':
+                parsed.append((line[0], False))
+            if line[1] == 'Bot':
+                parsed.append((line[0], True))
+            else:
+                print("parsing error: ", line)
 
-    return parsed_output
-
-# export the function
-def bodegha(token, repo):
-    return run_dockerfile(token, repo)
-
-# TODO handle errors on subprocess.run
+    return parsed
